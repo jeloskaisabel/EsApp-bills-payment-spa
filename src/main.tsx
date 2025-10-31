a7ec08a61cac2128c6e6ca38ee944fa302429d66
jeloskaisabel <jeloskaisabel@gmail.com>
2025-10-31 01:52:37 -0400

    refactor: replace App component with HomePage as entry point

--- src/main.tsx
+++ src/main.tsx
@@ -1,10 +1,11 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
 import './index.css'
-import App from './App.tsx'
+import './App.css'
+import { HomePage } from './pages/HomePage'
 
 createRoot(document.getElementById('root')!).render(
   <StrictMode>
-    <App />
+    <HomePage />
   </StrictMode>,
 )
